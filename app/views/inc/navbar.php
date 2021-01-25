d0a59f333775f3e347c7020af46eac5650f547eb
Karl-August Kongo <karl-august.kongo@khk.ee>
2021-01-25 11:34:11 +0200

    Loodud sisse logitud kasutaja navigatsiooni riba

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a href="<?php echo URLROOT; ?>" class="navbar-brand"><?php echo PROJECT_NAME ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                arial-expanded="false" arial-label="Toggle navigation">
+                aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -12,12 +12,18 @@
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
-                </li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/logout" class="nav-link">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
+                    </li>
+                <?php endif ?>
             </ul>
         </div>
     </div>
