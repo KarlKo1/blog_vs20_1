7e353ea0f76313ee94c51a19d377062b570a577b
Karl-August Kongo <karl-august.kongo@khk.ee>
2021-01-28 12:27:42 +0200

    Loodud üksiku postituse kuvamine

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -3,7 +3,7 @@
 <?php foreach ($data['posts'] as $post): ?>
     <div class="card card-body mb-3">
         <h3 class="card-title"><?php echo $post->title; ?></h3>
-        <div class="bg-light p-2 mb-3">Created by <?php echo $post->nam; ?> at <?php echo $post->postCreated; ?></div>
+        <div class="bg-light p-2 mb-3">Created by <?php echo $post->name; ?> at <?php echo $post->postCreated; ?></div>
 
         <p class="card-text"><?php echo $post->content; ?></p>
         <a href="<?php echo URLROOT ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-info">More</a>
